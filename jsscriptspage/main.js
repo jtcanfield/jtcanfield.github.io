5df800d7eddd8aa5b04b4f17fea7614f9c11d445
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-06-14 11:07:11 -0400

    Update main.js

--- jsscriptspage/main.js
+++ jsscriptspage/main.js
@@ -3,3 +3,9 @@ $(".html").hide();
 $(".html").fadeIn(2000);
 }
 $(document).ready(main);
+function incrementValue(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
