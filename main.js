e5689e8e564e41e20df8991428a8c410b819aae2
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-06-14 08:33:53 -0400

    Create main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(".html").fadeIn(1000);
 $(".topbar").hide();
 $(".topbar").fadeIn(2500);
 $(".topbar").on('click', function() { 
-    $(".topbar").fadeOut(1000);
-    //$(".topbar").hide();
+    //$(".topbar").fadeOut(1000);
+    $(".topbar").hide(1000);
   });
 }
 $(document).ready(main);
