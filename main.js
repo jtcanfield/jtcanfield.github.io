ec25209ce65586d876c9233788baee52bbd66a34
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-06-14 11:33:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function main() { 
+$(".div2").hide();
 $(".html").hide();
 $(".html").fadeIn(1000);
 $(".topbar").hide();
@@ -6,6 +7,10 @@ $(".topbar").fadeIn(2500);
 $(".topbar").on('click', function() { 
     //$(".topbar").fadeOut(1000);
     $(".topbar").hide(1000);
+  });
+$(".changepage").on('click', function() { 
+   $(".div1").slideToggle(1000);
+   $(".div2").slideToggle(1000);
   });
 }
 $(document).ready(main);
