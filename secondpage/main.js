bb9cb70e954e21b86d7cccde9b15d6ab8abc5b6e
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-06-15 10:41:25 -0400

    Create main.js

--- secondpage/main.js
+++ secondpage/main.js
@@ -3,3 +3,24 @@ $(".html").hide();
 $(".html").fadeIn(1000);
 }
 $(document).ready(main);
+$(document).ready(function(){
+$(".submitorder").click(function() {
+
+var sizeanswer = document.getElementById("sizeanswer");
+if(document.getElementById("sizelarge").checked) {
+sizeanswer.innerHTML= "Large Selected"; }
+if(document.getElementById("sizemedium").checked) {
+sizeanswer.innerHTML= "Medium Selected"; }
+if(document.getElementById("sizesmall").checked) {
+sizeanswer.innerHTML= "Small Selected"; }
+
+var crustanswer = document.getElementById("crustanswer");
+if(document.getElementById("crustthin").checked) {
+crustanswer.innerHTML= "Thin Selected"; }
+if(document.getElementById("crustthick").checked) {
+crustanswer.innerHTML= "Thick Selected"; }
+if(document.getElementById("crustfilled").checked) {
+crustanswer.innerHTML= "Filled Selected"; }
+
+});
+});
