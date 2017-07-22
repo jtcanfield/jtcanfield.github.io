064e621c42a51c3879c2758b2a5cc86b667e24f9
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-22 15:05:01 -0400

    Moved animations from java to CSS, corrected HTML spacing

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-function main() { 
+function main() {
 $(".div2").hide();
 $(".html").hide();
 $(".html").fadeIn(1000);
 $(".topbar").hide();
 $(".topbar").fadeIn(2500);
-$(".topbar").on('click', function() { 
+$(".topbar").on('click', function() {
     $(".topbar").hide(1000);
   });
-$(".changepage").on('click', function() { 
+$(".changepage").on('click', function() {
    $(".div1").slideToggle(1000);
    $(".div2").slideToggle(1000);
    $(".topbar").hide(1000);
   });
 }
 $(document).ready(main);
+/* Moved Below Scripts to CSS, kept for future reference
+this animation will always finish the first animation before doing the next one,
+so if you want something that keeps going after the user has finished interacting
+with it, use this.
 $(document).ready(function() {
    $('a').mouseenter(function() {
        $(this).animate({
@@ -23,6 +27,7 @@ $(document).ready(function() {
    $('a').mouseleave(function() {
        $(this).animate({
            padding: "-=10px",
-       }); 
+       });
    });
 });
+*/
