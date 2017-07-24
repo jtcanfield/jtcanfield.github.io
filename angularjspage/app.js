830bdca6c807f67f901a8d1522bdca1844646c32
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-24 11:43:19 -0400

    added basic js color changer

--- angularjspage/app.js
+++ angularjspage/app.js
@@ -1,5 +1,11 @@
 var app = angular.module("myApp", []);
 
-app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = "Jonathan's Angular Page"; 
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = "Jonathan's Angular Page";
+
 }]);
+function changeBackground() {
+    var color = document.getElementById("color").value;
+    document.getElementById("coltext").style.color = color;
+}
+document.getElementById("submitColor").addEventListener("click", changeBackground, false);
