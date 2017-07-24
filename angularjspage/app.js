fb527e8742b96fec8c5258303d9962930040c3da
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-24 12:13:04 -0400

    removed js page

--- angularjspage/app.js
+++ angularjspage/app.js
@@ -8,27 +8,3 @@ function changeBackground() {
     document.getElementById("coltext").style.color = color;
 }
 document.getElementById("submitColor").addEventListener("click", changeBackground, false);
-
-function main() {
-$(".html").hide();
-$(".html").fadeIn(1000);
-}
-$(document).ready(main);
-$(document).ready(function(){
-		$("#incrementbutton").click(function(){
-    var value = parseInt(document.getElementById('incrementnumber').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('incrementnumber').value = value;
-				});
-    $("#addbutton").click(function(){
-        var toAdd = $("input[name=checkListItem]").val();
-        $(".list").append("<div class='item'>" + toAdd + "</div>");
-        });
-     $("#deletebutton").click(function(){
-        $(".item").remove();
-        });
-     $(".list").on('click', '.item', function() {
-        $(this).remove();
-        });
-});
