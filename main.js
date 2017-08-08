7ced8db2ad3c5deac499da55b6f94b651ed3925b
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-08 09:29:44 -0400

    fix jquery

--- main.js
+++ main.js
@@ -2,16 +2,10 @@ function main() {
 $(".div2").hide();
 $(".html").hide();
 $(".html").fadeIn(1000);
-$(".topbar").hide();
-$(".topbar").fadeIn(2500);
-$(".topbar").on('click', function() {
-    $(".topbar").hide(1000);
-  });
 $(".changepage").on('click', function() {
    $(".div1").slideToggle(1000);
    $(".div2").slideToggle(1000);
-   $(".topbar").hide(1000);
-  });
+});
   window.requestAnimationFrame = window.requestAnimationFrame
    || window.mozRequestAnimationFrame
    || window.webkitRequestAnimationFrame
