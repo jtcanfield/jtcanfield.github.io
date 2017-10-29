f76a93c8c23007b18a21a1fc85a2b6f6f63023fe
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-28 20:30:34 -0400

    Add working parallax

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function main() {
-  window.requestAnimationFrame = window.requestAnimationFrame
-    || window.mozRequestAnimationFrame
-    || window.webkitRequestAnimationFrame
-    || window.msRequestAnimationFrame
-    || function(f){
-      setTimeout(f, 1000/60)
-    };
-}
-$(document).ready(main);
+window.requestAnimationFrame = window.requestAnimationFrame
+  || window.mozRequestAnimationFrame
+  || window.webkitRequestAnimationFrame
+  || window.msRequestAnimationFrame
+  || function(f){setTimeout(f, 1000/60)};
+  var parallax1 = document.getElementsByTagName('body')[0];
+  function parallaxeffect(){
+    var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+    parallax1.style.backgroundPosition = "0px " + -scrolltop * .5 + 'px'
+  };
+  window.addEventListener('scroll', function(){ // on page scroll
+    requestAnimationFrame(parallaxeffect) // call parallaxeffect() every screen paint
+  }, false);
